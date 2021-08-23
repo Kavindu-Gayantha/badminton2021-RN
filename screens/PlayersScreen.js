3f5af5dcdb95720619d1a331cd8d43887b39e746
kavindu-gayantha <96kavindugayantha@gmail.com>
2021-08-23 12:52:13 +0530

    now http working

--- screens/PlayersScreen.js
+++ screens/PlayersScreen.js
@@ -1,26 +1,69 @@
 import * as React from "react";
 import {useEffect} from "react";
-import {Text} from "react-native";
-import {supabase} from "../superbaseInit";
+import {Text,View, StyleSheet, Button} from "react-native";
+import { supabase } from "../lib/superbaseInit";
+import { useState } from "react";
+import axios from "axios";
+// import { createClient } from '@supabase/supabase-js'
+
+// const supabaseUrl = 'https://txxjtlrnmxsxpnwaemti.supabase.co'
+// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzkxMjAzNCwiZXhwIjoxOTQzNDg4MDM0fQ.vxGyYCwLx5RaGF8UZbLxWv1ccZIiT4cwVTxss3nF0y4'
+// const supabase = createClient(supabaseUrl, supabaseKey)
 
 const PlayersScreen = () => {
-    const getAllPlayers = async () => {
-        try {
+const [players, setPlayers] = useState([]);
 
-            let {data} = await supabase
-                .from('players')
-                .select('*')
-
-            console.log("all players", JSON.parse(data));
-        } catch (error) {
-            console.log('error ***********', error);
-        }
+//     const getAllPlayers = async () => {
+//          try {
+        
+//              let {data: players, error} = await supabase
+//                  .from('faculty')
+//                  .select('*')
+//                  if(error) {
+// console.log(error)
+//         } else {
+//                      return players;
+//                  } 
+//             //  console.log("all players", JSON.parse(players));
+//             //  return data;
+//          } catch (error) {
+//              console.log('error ***********', error);
+//          }
+//     }
+    const onPress = () => {
+        axios.get("http://192.168.118.115:8080/badminton/players/getAll").then(data => {
+            console.log('data *********', JSON.parse(data));
+        }).catch(error => {
+            console.log('error', error);
+        })
     }
     useEffect(()=> {
-        getAllPlayers();
+        // getAllPlayers().then( data => {
+        //     console.log('data', data);
+        // });
     })
     return (
-        <Text>PlayersScreen</Text>
+        <View style={styles.container}>
+            <Text style={styles.title}>Hi</Text>
+            <Button title="ppppp" onPress={onPress}> PRESS</Button>
+        </View>
     );
 }
 export default PlayersScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '12%',
+    },
+    title: {
+        fontWeight: 'bold',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        flex: 1,
+        marginTop: 5
+    }
+});
--- superbaseInit.js
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://txxjtlrnmxsxpnwaemti.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyNzkxMjAzNCwiZXhwIjoxOTQzNDg4MDM0fQ.vxGyYCwLx5RaGF8UZbLxWv1ccZIiT4cwVTxss3nF0y4'
-export const supabase = createClient(supabaseUrl, supabaseKey)
