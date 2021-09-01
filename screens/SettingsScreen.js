6a805c63a6628861da63e9f85fa8d2c1ddb5555c
kavindu-gayantha <96kavindugayantha@gmail.com>
2021-09-01 22:05:27 +0530

    settings screen done

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,9 +1,125 @@
 import * as React from "react";
-import {Text} from "react-native";
+import {useEffect, useState} from "react";
+import {Text, View, StyleSheet, TouchableOpacity, TextInput} from "react-native";
+import { ListItem, Icon, Overlay, Card } from 'react-native-elements'
 
+const list = [
+    {
+        title: 'Edit Profile',
+        icon: 'edit'
+    },
+    {
+        title: 'Help',
+        icon: 'help'
+    },
+    {
+        title: 'Log out',
+        icon: 'logout'
+    },
+    {
+        title: 'About',
+        icon: 'info'
+    },
+    
+];
+// const editListItemPressed = (item) => {
+//     console.log("itemp pressed: ", item);
+//     // switch (item) {
+//     //     case 'About': {
+//     //         return toggleOverlay();
+//     //     }
+//     // }
+//     if (item === "About") {
+//      toggleOverlay();
+//     }
+// }
+// const toggleOverlay = () => {
+//         setVisible(!visible);
+//     };
+// const [visible, setVisible] = useState(false);
 const SettingsScreen = () => {
+    const [visible, setVisible] = useState(false);
+
+    const toggleOverlay = () => {
+        setVisible(!visible);
+    };
+
+    const editListItemPressed = (item) => {
+    console.log("itemp pressed: ", item);
+    // switch (item) {
+    //     case 'About': {
+    //         return toggleOverlay();
+    //     }
+    // }
+    if (item === "About") {
+     toggleOverlay();
+    }
+}
     return(
-        <Text>SettingsScreen</Text>
+        <View style={styles.container}>
+            {
+                list.map((item, i) => (
+                    <ListItem key={i} bottomDivider onPress={()=> editListItemPressed(item.title)}>
+                        <Icon name={item.icon} />
+                        <ListItem.Content>
+                        <ListItem.Title>{item.title}</ListItem.Title>
+                        </ListItem.Content>
+                        <ListItem.Chevron />
+                    </ListItem>
+                ))
+            }
+            <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+                <Card>
+                    <Card.Title style={{fontSize: 20}}>Made By:</Card.Title>
+                    <Text style={{textAlign: 'center'}}>Kavindu Gayantha</Text>
+                    <Text style={{ textAlign: 'center' }}>Software Engineering Undergraduate</Text>
+                    <Text style={{textAlign: 'center'}}> University of Kelaniya </Text>
+                </Card>
+            </Overlay>
+            </View>
     );
 }
 export default SettingsScreen;
+
+const styles = StyleSheet.create({
+    listContainer: {
+        backgroundColor: '#134717',
+        // padding: 5,
+        // margin: 1,
+        height: 50,
+        // flex: 1,
+        color: 'white',
+        flexDirection: 'row',
+        justifyContent: 'center'
+    },
+    container: {
+        backgroundColor: '#98ee99',
+    },
+    textWrap: {
+        flex: 9,
+        padding: 1,
+        // fontSize: 15,
+        justifyContent: 'center',
+        alignItems: 'flex-start',
+        // fontWeight: 'bold'
+    },
+    iconWrap: {
+      flex: 1,
+      padding: 1,
+      justifyContent: 'center',
+      alignItems: 'flex-start'
+    
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 15,
+    },
+    overlayTextWrap: {
+        justifyContent: 'center',
+        textAlign: 'center',
+        alignContent: 'center',
+        alignSelf: 'center',
+        flex: 1
+    }
+  
+})
