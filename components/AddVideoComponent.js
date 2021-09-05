e6c51087d922cf7eebbb2fe76f111b43c603d9d5
kavindu-gayantha <96kavindugayantha@gmail.com>
2021-09-05 13:41:47 +0530

    add video done

--- components/AddVideoComponent.js
+++ components/AddVideoComponent.js
@@ -1,21 +1,64 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import axios from "axios";
+import { BASE_URL } from "../api/BASE_URL";
 
-const AddVideoComponent = () => {
+const AddVideoComponent = (props) => {
+  // console.log("props add new vide : ", props);
   const [urlLink, setUrlLink] = useState("");
 
-  const addYoutubeLink = () => {
-    const videoId = convertLinkIntoYoutubeVideoId(urlLink);
+  const onSubmiYoutubeLink = () => {
+    // console.log("youuuuu: ", urlLink);
+    const body = {
+      youtubeLink: urlLink,
+    };
+    if (body.youtubeLink !== "") {
+      addYoutubeLink(body).then(() => setUrlLink(""));
+    }
+  };
+
+  const convertYoutubeLinkToYoutubeId = (response) => {
+    const youtubeLink = response.youtubeLink.toString();
+    // console.log("responsessssss: ", youtubeLink);
+
+    const videoId = youtubeLink.substring(17, youtubeLink.length);
+    console.log("id:  ", videoId);
+
+    if (videoId) {
+      const videoNewObj = {
+        id: response.id,
+        youtubeLink: videoId,
+      };
+      props.addNewVideo(videoNewObj);
+    }
+  };
+
+  //Create player API here
+  const addYoutubeLink = async (body) => {
+    try {
+      const request = await axios.post(`${BASE_URL}/video/create`, body);
+      // setShowSubmitTxt(true);
+      // setResponseMessage(request.data.statusMessage);
+      setUrlLink(request.data.data);
+      convertYoutubeLinkToYoutubeId(request.data.data);
+      console.log("request response", request.data.statusMessage);
+    } catch (error) {
+      console.log(error);
+    }
   };
   return (
     <View style={styles.addVideoContainer}>
-      <TextInput
-        placeholder="First Name"
-        onChangeText={(value) => setUrlLink(value)}
-        value={urlLink}
-        style={styles.inputBox}
-      />
-      <Button title="OK" onPress={addYoutubeLink} />
+      <View style={styles.urlInputView}>
+        <TextInput
+          placeholder="Youtube Video Link"
+          onChangeText={(value) => setUrlLink(value)}
+          
+          style={styles.inputBox}
+        />
+      </View>
+      <View style={styles.urlInputViewBtn}>
+        <Button title="OK" onPress={onSubmiYoutubeLink} />
+      </View>
     </View>
   );
 };
@@ -23,5 +66,21 @@ const AddVideoComponent = () => {
 export default AddVideoComponent;
 
 const styles = StyleSheet.create({
-  inputBox: {},
+  inputBox: {
+    borderColor: "white",
+    color: "green",
+  },
+  addVideoContainer: {
+    margin: 5,
+    padding: 5,
+    flex: 1 / 9,
+    // borderColor: 'red',
+    flexDirection: "row",
+  },
+  urlInputView: {
+    flex: 3,
+  },
+  urlInputViewBtn: {
+    flex: 1,
+  },
 });
